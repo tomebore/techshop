5f07f6e63775731cdf4fe1963202b9349bbe05d8
tomebore <akylaimanalieva@gmail.com>
2020-07-12 14:35:37 +0600

    added static and media settings and urls

--- readme.txt
+++ readme.txt
@@ -1,2 +1,4 @@
-Please add settings.py in techsop folder withut special parameters 
-but add core , product and feedback into Inatalled_apps
+Please add settings.py in techsop folder :
+1) add core , product and feedback into Inatalled_apps
+2) add STATIC_URL , STATIC_ROOT , MEDIA_URL , MEDIA_ROOT
+
--- techshop/urls.py
+++ techshop/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("product/", include('product.urls')),
     path("feedback/", include('feedback.urls')),
     path("", include('core.urls')),
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+ + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
