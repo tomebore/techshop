e117f0dbcb1ec448f68706f3a2fb7bfd867f4dba
tomebore <akylaimanalieva@gmail.com>
2020-07-13 17:07:23 +0600

    added product page

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include
 from django.urls import path
-from .views import products
+from .views import *
 
 urlpatterns = [
     path("all/", products , name="products"),
+    path("<int:id>/", product , name="product"),
 ]
--- product/views.py
+++ product/views.py
@@ -6,3 +6,8 @@ def products(request):
     context = {}
     context["products"] =  Product.objects.filter(aviable=True)
     return render(request , "product/products.html", context)
+
+def product(request ,id):
+    context = {}
+    context["product"] = Product.objects.get(id=id)
+    return render(request , "product/product.html" ,context)
