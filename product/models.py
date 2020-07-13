e37473c1ba599b730efcb92c506500a991ea68aa
tomebore <akylaimanalieva@gmail.com>
2020-07-13 15:15:21 +0600

    added product.category and base.html

--- product/models.py
+++ product/models.py
@@ -4,25 +4,47 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(
-        max_length=255 , verbose_name="Название")
+        max_length = 255 , verbose_name = "Название")
+
+    category = models.ForeignKey(
+        "Category" , 
+        on_delete = models.SET_NULL,
+        related_name = "product",
+        null = True ,
+        blank = True,
+        verbose_name = "Категория")
 
     description =models.TextField(
-        null=True , blank=True ,verbose_name="Описание")
-
+        null = True , blank = True ,verbose_name = "Описание")
+ 
     price = models.DecimalField(
-        max_digits=11,
-        decimal_places=3,
-        verbose_name="Цена")
+        max_digits = 11,
+        decimal_places = 3,
+        verbose_name = "Цена")
 
     salse =models.IntegerField(
-        default=0 , verbose_name="Кол-во продаж")
+        default = 0 , verbose_name = "Кол-во продаж")
 
     aviable = models.BooleanField(
-        default=True, verbose_name="Есть в наличии")
+        default = True, verbose_name = "Есть в наличии")
+
+
 
     def __str__(self):
         return self.name 
 
     class Meta:
-        verbose_name = "Товар"
+        verbose_name = "товар"
         verbose_name_plural = "Товары"
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length = 250 , verbose_name = "Название")
+
+    def __str__(self):
+        return self.name
+
+    class Meta :
+        verbose_name = "категория"
+        verbose_name_plural = "Категории"
