687967a17a4d0b0609d25ab5b383dbb8f2b7166e
tomebore <akylaimanalieva@gmail.com>
2020-09-27 02:00:30 +0600

    added product edit

--- product/views.py
+++ product/views.py
@@ -24,3 +24,18 @@ def product_create(request):
     context["form"] = ProductForm
 
     return render(request,"product/form.html",context)
+
+def product_edit(request , id):
+    product = Product.objects.get(id=id)
+
+    if request.method == "POST":
+        form = ProductForm(request.POST,instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect("product",id=product.id)
+    
+
+    context = {}
+    context["form"] = ProductForm(instance=product)
+
+    return render(request,"product/form.html", context)
