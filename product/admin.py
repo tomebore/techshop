6e4e6bc74190f4dca3155f6726bf811d3b96e1c7
Tomebore <akylaimanalieva@gmail.com>
2021-03-19 13:06:26 +0600

    requirements.txt changed

--- product/admin.py
+++ product/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from product.models import *
 # Register your models here.
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    # fields = [field.name for field in Product._meta.get_fields()[1:]]
+    readonly_fields = ["user", "sales", "price", "avialable", "deleted"]
+    list_display = [field.name for field in Product._meta.get_fields()]
+    list_display_links = ("id", "name", "user")
+    list_editable = ("price",)
+    search_fields = ["name", "description", "user__username"]
+    list_filter = ["category", "name"]
+    list_per_page = 10
+
 admin.site.register(Category)
